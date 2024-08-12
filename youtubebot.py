2a00e1c1fc2ff88a7357d72c883adb0b693718e1
valdife <salemon876@gmail.com>
2024-08-12 21:56:10 +0200

    Remove any cookies from browser

--- youtubebot.py
+++ youtubebot.py
@@ -103,6 +103,7 @@ async def play(ctx: commands.Context, *args):
                            'outtmpl': '%(id)s.%(ext)s',
                            'noplaylist': True,
                            'allow_playlist_files': False,
+                           'cookiesfrombrowser': None,
                            # 'progress_hooks': [lambda info, ctx=ctx: video_progress_hook(ctx, info)],
                            # 'match_filter': lambda info, incomplete, will_need_search=will_need_search, ctx=ctx: start_hook(ctx, info, incomplete, will_need_search),
                            'paths': {'home': f'./dl/{server_id}'}}) as ydl:
